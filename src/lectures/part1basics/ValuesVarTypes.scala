b14b9fc8131d61cdeebd417fa9e55540496fb440
rdwentmp <maaarek7823@gmail.com>
2021-06-28 15:09:35 +0200

    expression

--- src/lectures/part1basics/ValuesVarTypes.scala
+++ src/lectures/part1basics/ValuesVarTypes.scala
@@ -7,6 +7,19 @@ object ValuesVarTypes extends App {
 
  val aString: String = "Hello"
   val anotherString: String = "Bye"
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val aInt: Int = x
+  val aShort: Short = 4613
+  val aLong: Long = 5398279873892L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.23
+
+  // Variables
+
+  var aVariable: Int = 4
+
+  aVariable = 5
 
 }
 
