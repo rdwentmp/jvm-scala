25259600d73d26f73b63ebce295a102b66f9be60
rdwentmp <maaarek7823@gmail.com>
2021-06-28 14:41:52 +0200

    first commit

---
+++ src/lectures/part1basics/ValuesVarTypes.scala
@@ -0,0 +1,12 @@
+package lectures.part1basics
+
+object ValuesVarTypes extends App {
+  val x: Int = 23
+  println(x)
+  // Vals are immutable
+
+ val aString: String = "Hello"
+  val anotherString: String = "Bye"
+
+}
+
