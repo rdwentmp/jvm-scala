778bb2f33aa1f2b910a109dd3b7ab77da83dda07
rdwentmp <maaarek7823@gmail.com>
2021-06-29 12:48:12 +0200

    rm cached

--- src/lectures/part1basics/Greet.scala
+++ src/lectures/part1basics/Greet.scala
@@ -6,4 +6,28 @@ object Greet extends App {
   "Hi, my name is " + name + " and I am " + age + " years old."
    println(greeting("David", 1))
 
-}
+ def factorial(n: Int): Int =
+  if (n <= 0) 1
+  else n * factorial(n-1)
+
+ println(factorial(5))
+
+ def fibonacci(n: Int): Int =
+  if (n <= 2) 1
+  else fibonacci(n-1) + fibonacci(n-2)
+
+ //  1 1 2 3 5 8 13 21
+ println(fibonacci(8))
+
+ def isPrime(n: Int): Boolean = {
+  def isPrimeUntil(t: Int): Boolean =
+   if (t <= 1) true
+   else n % t != 0 && isPrimeUntil(t - 1)
+
+  isPrimeUntil(n / 2)
+  }
+    println(isPrime(37))
+    println(isPrime(2003))
+    println(isPrime(37 * 13))
+
+ }
