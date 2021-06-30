6f242ca74181036cb77e3b3bc07499f77ead6c1b
rdwentmp <maaarek7823@gmail.com>
2021-06-30 10:32:35 +0200

    fib func

--- src/lectures/part1basics/Recursion.scala
+++ src/lectures/part1basics/Recursion.scala
@@ -1,5 +1,7 @@
 package lectures.part1basics
 
+import scala.annotation.tailrec
+
 object Recursion extends App {
   def factorial(n: Int): Int =
     if (n <= 1) 1
@@ -20,5 +22,39 @@ object Recursion extends App {
     factHelper(n, 1)
   }
     println(anotherFactorial(10000))
+
+  // when you need loops use TAIL_RECURSION
+  /*
+    1. CONCATENATE A STRING N TIMES
+    2. Is Prime function tail recursive
+    3. Fibonacci function, tail recursive
+   */
+  @tailrec
+  def concatenateTailrec(aString: String, n: Int, accumulator: String): String =
+    if (n <= 0) accumulator
+    else concatenateTailrec(aString, n-1, aString + accumulator)
+
+  println(concatenateTailrec("hello", 3, ""))
+
+//  def isPrime(n: Int): Boolean = {
+//    @tailrec
+//    def isPrimeTailrec(t: Int, isStillPrime: Boolean): Boolean = {
+//      if (!isStillPrime) false
+//      else if (t <= 1) true
+//      else isPrimeTailrec(t - 1, n % t != 0 && isStillPrime)
+//
+//      isPrimeTailrec(n / 2, true)
+//    }
+//    println(isPrime(2003))
+//    println(isPrime(256))
+  def fibonacci(n: Int): Int = {
+   def fiboTailrec(i: Int, last: Int, nextToLast: Int): Int =
+     if(i >= n) last
+     else fiboTailrec(i + 1, last + nextToLast, last)
+
+  if (n <= 2 ) 1
+  else fiboTailrec(2, 1, 1)
+}
+  println(fibonacci(8))
 
 }
