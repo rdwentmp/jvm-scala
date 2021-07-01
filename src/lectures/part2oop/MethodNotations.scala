64710f7da4ad91b80413c30a88b512e27a85f468
rdwentmp <maaarek7823@gmail.com>
2021-07-01 16:09:01 +0200

    Postfix notation and equivalent

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -3,9 +3,11 @@ package lectures.part2oop
 object MethodNotations extends App {
 
   class Person(val name: String, favouriteMovie: String) {
-
      def likes(movie: String): Boolean = movie == favouriteMovie
-    def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+     def unary_! : String = s"$name, what the heck?!"
+     def isAlive: Boolean = true
+     def apply(): String = s"Hi my is $name and I like $favouriteMovie"
   }
 
   val mary = new Person("Marry", "Inception")
@@ -19,7 +21,23 @@ object MethodNotations extends App {
   println(mary + tom)
   println(mary.+(tom))
 
+  println(1 + 2)
+  println(1.+(2))
+  // ALL OPERATORS ARE METHODS
 
+  //prefix notation
+  val x = -1
+  val y = 1.unary_-  // unary_ prefix works with + - ~ !
+  println(x == y)
 
+  println(!mary)
+  println(mary.unary_!)
 
+  // postfix notation
+  println(mary.isAlive)
+  println(mary isAlive)
+
+  // apply
+  println(mary.apply())
+  println(mary()) // equivalent
 }
