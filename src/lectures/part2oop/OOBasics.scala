19c87863f5e4cfe3c436960277f7f9e72fc7d334
rdwentmp <maaarek7823@gmail.com>
2021-07-01 12:28:13 +0200

    def counter inc/dec

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -7,6 +7,17 @@ object OOBasics extends App {
   person.greet("Daniel")
   person.greet()
 
+  val author = new Writer("Charles", "Dickens", 1812)
+  val imposter = new Writer("Charles", "Dickens", 1812)
+  val novel = new Novel("Great Expectations", 1861, author)
+
+  println(novel.authorAge)
+  println(novel.isWrittenBy(imposter))
+
+  val counter = new Counter
+  counter.inc.print
+  counter.inc.inc.inc.print
+  counter.inc(10).print
 }
 // Constructor
 class Person(name: String, val age: Int = 0) {
@@ -23,6 +34,55 @@ class Person(name: String, val age: Int = 0) {
   // multiple constructors
   def this(name: String) = this(name, 0)
   def this() = this("John Doe")
+}
+
+/*
+  Novel and a Writer
+
+  Writer: first name, surname, year
+   - method fullname
+
+   Novel: name, year of release, author
+   - authorAge
+   - isWrittenBy (author)
+   - copy (new year of release) = new instance of Novel
+*/
+class Writer(firstName: String, surnane: String, val year: Int) {
+  def fullName: String = firstName + " " + surnane
+  }
+
+class Novel(name: String, year: Int, author: Writer) {
+  def authorAge = year - author.year
+  def isWrittenBy(author: Writer) = author == this.author
+  def copy(newYear: Int): Novel = new Novel(name, newYear, author)
+
+}
+/*
+   Counter class
+   - recives an int value
+   - method to increment/ decrement => new Counter
+   - overload ind dec to recieve an amount
+*/
+class Counter(val count: Int = 0) {
+  def inc = {
+    println("incrementing")
+    new Counter(count + 1) // immutability
+    }
+  def dec = {
+    println("decrementing")
+    new Counter(count - 1)
+  }
+  def inc(n: Int): Counter = {
+    if (n <= 0 ) this
+    else inc.inc(n - 1)
+  }
+  def dec(n: Int): Counter = {
+  if (n <= 0) this
+  else dec.dec(n - 1)
+  }
+
+  def print = println(count)
+
 }
 
 // class parameters are not fields
