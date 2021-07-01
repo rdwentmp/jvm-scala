92c93f49d6f7ba712b55bb95b3634a496cc1dd9d
rdwentmp <maaarek7823@gmail.com>
2021-07-01 09:18:59 +0200

    multiple constructors

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -2,15 +2,28 @@ package lectures.part2oop
 
 object OOBasics extends App {
 
-  val person = new Person("David", 27)
-  println(person.age)
+  val person = new Person("John", 27)
+  println(person.x)
+  person.greet("Daniel")
+  person.greet()
 
 }
 // Constructor
-class Person(name: String, val age: Int) {
+class Person(name: String, val age: Int = 0) {
   // body
    val x = 2
-
   println(x + 2)
+
+  // method
+  def greet(name: String): Unit = println(s"${this.name} says: Hi, $name")
+
+  // overloading
+  def greet(): Unit = println(s"Hi, I am $name")
+
+  // multiple constructors
+  def this(name: String) = this(name, 0)
+  def this() = this("John Doe")
 }
+
+// class parameters are not fields
 
