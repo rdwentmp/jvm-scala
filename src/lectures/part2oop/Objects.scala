dcd9e49aa40c045b3ddb5b7abde9f1c237b99605
rdwentmp <maaarek7823@gmail.com>
2021-07-02 15:39:15 +0200

    scala applications

--- src/lectures/part2oop/Objects.scala
+++ src/lectures/part2oop/Objects.scala
@@ -7,17 +7,26 @@ object Objects extends App {
   // "static" class - level functionality
     val N_EYES = 2
     def canFly: Boolean = false
+
+    // factory method
+    def apply(mother: Person, father: Person): Person = new Person("Alex")
   }
-  class Person {
+  class Person(val name: String) {
     // instance level functionality
   }
   //COMPANIONS
   println(Person.N_EYES)
   println(Person.canFly)
   // Scala Objects = SINGLETON INSTANCE
-  val mary = Person
-  val john = Person
+  val mary = new Person("Marry")
+  val john = new Person("John")
   println(mary == john)
+
+  val alex = Person.apply(mary, john)
+
+
+  // Scala applications = Scala Objects with
+  // def main(args: Array:[String]): Unit
 
 
 }
