560f4805a3101df2aa11e6826a77f7183552fcdd
rdwentmp <maaarek7823@gmail.com>
2021-07-05 14:39:11 +0200

    preventing ovverride

--- src/lectures/part2oop/Inheritance.scala
+++ src/lectures/part2oop/Inheritance.scala
@@ -1,7 +1,8 @@
 package lectures.part2oop
 
 object Inheritance extends App {
-    class Animal {
+    sealed class Animal {
+      val creatureType = "wild"
       def eat = println("ohomomo")
     }
     // Single class inheritance
@@ -12,7 +13,7 @@ object Inheritance extends App {
       }
     }
     val cat = new Cat
-    cat.eat
+    cat.crunch
 
     // constructors
     class Person(name: String, age: Int) {
@@ -21,7 +22,28 @@ object Inheritance extends App {
     class Adult(name: String, age: Int, idCard: String) extends Person(name)
 
     // overriding
-    class Dog extends Animal {
+    class Dog(override val creatureType: String) extends Animal {
 
+      override def eat = {
+        super.eat
+        println("crunch crunch")
+      }
     }
+     val dog = new Dog("k9")
+    dog.eat
+    println(dog.creatureType)
+
+    //type substitution (broad: polymorphism)
+  val unknownAnimal: Animal = new Dog("k9")
+  unknownAnimal.eat
+
+  // overriding vs overloading
+
+  // super
+
+  // Preventing override
+  // 1 use final on member
+  // 2 use final on a class
+  // 3 seal the class = extends classes in this file, prevent extension in other files
+
     }
