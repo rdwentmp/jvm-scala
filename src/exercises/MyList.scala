174d0de7895f6b092f9473a17590149ede0ca690
rdwentmp <maaarek7823@gmail.com>
2021-07-06 13:23:45 +0200

    access modifiers and override

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -14,6 +14,9 @@ abstract class MyList {
   def tail: MyList
   def isEmpty: Boolean
   def add(element: Int): MyList
+  def printElements: String
+  // polymorphic call
+  override def toString: String = "[" + printElements + "]"
 }
 
 object Empty extends MyList {
@@ -21,6 +24,7 @@ object Empty extends MyList {
   def tail: MyList = throw new NoSuchElementException
   def isEmpty: Boolean = true
   def add(element: Int): MyList = new Cons(element, Empty)
+  def printElements: String = ""
 }
 
 class Cons(h: Int, t: MyList) extends MyList {
@@ -28,10 +32,18 @@ class Cons(h: Int, t: MyList) extends MyList {
   def tail: MyList = t
   def isEmpty: Boolean = false
   def add(element: Int): MyList = new Cons(element, this)
+  def printElements: String =
+    if (t.isEmpty) "" + h
+    else h + " " + t.printElements
 }
 
 object ListTest extends App {
-  val list = new Cons(1, Empty)
-  println(list.head)
+  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  println(list.tail.head)
+  println(list.add(4).head)
+  println(list.isEmpty)
+
+
+  println(list.toString)
 }
 
