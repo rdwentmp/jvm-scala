5de6bf094d39b51260418f7fed57fa2681a2ac11
rdwentmp <maaarek7823@gmail.com>
2021-07-08 11:25:50 +0200

    OOM exception

--- src/lectures/part2oop/Exceptions.scala
+++ src/lectures/part2oop/Exceptions.scala
@@ -17,15 +17,43 @@ object Exceptions extends App {
     if (withExceptions) throw  new RuntimeException("No int for you")
     else 42
 
-  try {
+  val potentialFail = try {
     // code that might fail
     getInt(true)
   } catch {
-    case e: RuntimeException => println("caught a Runtime exception")
+    case e: RuntimeException => 43
   } finally  {
     // code that will get executed no matter what
+    // optional
+    // does not influence the return type of this expression
+    // use finally only for side efect
     println("finally")
   }
+  println(potentialFail)
+
+  // 3. how to define your own exceptions
+
+  class MyException extends Exception
+  val exception = new MyException
+
+//  throw exception
+  /*
+    1. Crash youor program with an OutOfMemoryError
+    2. Crash with SOError
+    3. Pocket calculator
+        - add(x,y)
+        - subtract(x,y)
+        - multiple(x,y)
+        -divide(x,y)
+
+        Throw
+        - OverflowException id add(x,y) exceeds Int.MAX_VALUE
+        - UnderflowException if subtract(x,y) exceds Int.MIN_VALUE
+        - MatchCalculationException for division by 0
+   */
+  // OOM
+//  val array = Array.ofDim(Int.MaxValue)
+
 
 
 }
