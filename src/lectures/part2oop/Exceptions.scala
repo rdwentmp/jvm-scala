71e6d5bf5eb05a02eead5ed5ce845b052dcdf33f
rdwentmp <maaarek7823@gmail.com>
2021-07-08 15:25:43 +0200

    math calculation exceptions

--- src/lectures/part2oop/Exceptions.scala
+++ src/lectures/part2oop/Exceptions.scala
@@ -57,13 +57,40 @@ object Exceptions extends App {
   // SO
 //  def infinite: Int = 1 + infinite
 //  val noLimit = infinite
+  class OverflowException extends RuntimeException
+  class UnderflowException extends RuntimeException
+  class MathCalculationException extends RuntimeException("Division by 0")
 
   object PocketCalculator {
     def add(x: Int, y: Int) = {
       val result = x + y
-      result
+      if (x > 0 && y > 0 && result < 0) throw new OverflowException
+      else if (x < 0 && y < 0 && result > 0) throw new UnderflowException
+      else result
+    }
+
+    def subtract(x: Int, y: Int) = {
+      val result = x - y
+      if (x > 0 && y < 0 && result < 0) throw new OverflowException
+      else if (x < 0 && y > 0 && result > 0) throw new UnderflowException
+      else result
+    }
+
+    def multiply(x: Int, y: Int) = {
+      val result = x * y
+      if (x > 0 && y > 0 && result < 0) throw new OverflowException
+      else if (x < 0 && y < 0 && result < 0) throw new UnderflowException
+      else if (x > 0 && y < 0 && result > 0) throw new UnderflowException
+      else if (x < 0 && y > 0 && result > 0) throw new UnderflowException
+      else result
+    }
+
+    def divide(x: Int, y: Int) = {
+      if (y == 0) throw new MathCalculationException
+      else x / y
     }
   }
-  println(PocketCalculator.add(Int.MaxValue, 10))
+//  println(PocketCalculator.add(Int.MaxValue, 10))
+    println(PocketCalculator.divide(2,0))
 
 }
