27d7c6a7e3d19e9a530cab8a884baf8dcd44fd35
rdwentmp <maaarek7823@gmail.com>
2021-07-21 15:37:44 +0200

    curried functions with multiple parameter lists

--- src/lectures/part3fp/HOFsCurries.scala
+++ src/lectures/part3fp/HOFsCurries.scala
@@ -25,6 +25,22 @@ object HOFsCurries extends App {
   val plus10 = nTimesBetter(plusOne, 10)
   println(plus10(1))
 
+  // curried functions
+  val superAdder: Int => Int => Int = (x: Int) => (y: Int) => x + y
+  val add3 = superAdder(3) // y => 3+y
+  println(add3(10))
+  println(superAdder(3)(10))
+
+  // functions with multiple parameter lists
+  def curriedFormatter(c: String)(x: Double): String = c.format(x)
+
+  val standardFormat: (Double => String) = curriedFormatter("%4.2f")
+  val preciseFormat: (Double => String) = curriedFormatter("%10.8f")
+
+  println(standardFormat(Math.PI))
+  println(preciseFormat(Math.PI))
+
+
 
 
 
