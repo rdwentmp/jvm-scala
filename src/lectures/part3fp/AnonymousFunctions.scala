c468c419efea5372fa439650a74d18b72e718d52
rdwentmp <maaarek7823@gmail.com>
2021-07-21 11:34:04 +0200

    curried functions

--- src/lectures/part3fp/AnonymousFunctions.scala
+++ src/lectures/part3fp/AnonymousFunctions.scala
@@ -3,10 +3,10 @@ package lectures.part3fp
 object AnonymousFunctions extends App {
 
  // anonymous function (LAMBDA)
-  val doubler: Int => Int = (x: Int) => x * 2
+ val doubler: Int => Int = (x: Int) => x * 2
 
-  // multiple params in a lambda
-  val adder: (Int, Int) => Int = (a: Int, b: Int) => a + b
+ // multiple params in a lambda
+ val adder: (Int, Int) => Int = (a: Int, b: Int) => a + b
 
  // no params
  val justDoSomething: () => Int = () => 3
@@ -16,16 +16,21 @@ object AnonymousFunctions extends App {
 
  // curly braces with lambdas
  val stringTo = { (str: String) =>
-   str.toInt
+  str.toInt
  }
 
  // More syntactic sugar
  val niceIncrementer: Int => Int = _ + 1 // equivalent to x => x + 1
  val niceAdder: (Int, Int) => Int = _ + _ // equivalent to (a,b) => a + b
-}
+
  /*
   1. MyList: replace all FunctionX calls with the lambdas
   2. Rewrite the "special" adder as an anonymous function
   */
 
+ val superAdd = (x: Int) => (y: Int) => x + y
+ println(superAdd(3)(4))
+
+
+}
 
