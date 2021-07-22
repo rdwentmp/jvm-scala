532b55a26958d94cfde96bc705977af00a057d27
rdwentmp <maaarek7823@gmail.com>
2021-07-22 12:43:58 +0200

    finish composed exercise

--- src/lectures/part3fp/HOFsCurries.scala
+++ src/lectures/part3fp/HOFsCurries.scala
@@ -59,4 +59,33 @@ object HOFsCurries extends App {
      3. compose(f,g) => x => f(g(x))
         andThen(f,g) => x => g(f(x))
    */
+
+  def toCurry(f: (Int, Int) => Int): (Int => Int => Int) =
+    x => y => f(x, y)
+
+  def fromCurry(f: ( Int => Int => Int)): (Int, Int) => Int =
+    (x, y) => f(x)(y)
+
+  // FunctionX
+  def compose[A,B,T](f: A => B, g: T => A): T => B =
+    x => f(g(x))
+
+  def andThen[A,B,C](f: A => B, g: B => C): A => C =
+    x => g(f(x))
+
+  def superAdder2: (Int => Int => Int) = toCurry(_ + _)
+  def add4 = superAdder(4)
+  println(add4(17))
+
+  val simpleAdder = fromCurry(superAdder)
+  println(simpleAdder(4,17))
+
+  val add2 = (x: Int) => x + 2
+  val times3 = (x: Int) => x * 3
+
+  val composed = compose(add2, times3)
+  val ordered = andThen(add2, times3)
+
+  println(composed(4))
+  println(ordered(4))
 }
