92f1379a266d522c96bb71007c6a7924536e6f29
rdwentmp <maaarek7823@gmail.com>
2021-07-29 12:20:22 +0200

    friends network

--- src/lectures/part3fp/TuplesAndMaps.scala
+++ src/lectures/part3fp/TuplesAndMaps.scala
@@ -12,7 +12,7 @@ object TuplesAndMaps extends App {
   // Maps - keys -> value
   val aMap: Map[String, Int] = Map()
 
-  val phonebook = Map(("Jim", 555), "Mark" -> 698).withDefaultValue(-1)
+  val phonebook = Map(("Jim", 555), "Mark" -> 698, ("JIM", 9000)).withDefaultValue(-1)
   // a -> b is sugar for (a,b)
   println(phonebook)
 
@@ -40,8 +40,38 @@ object TuplesAndMaps extends App {
   println(phonebook.toList)
   println(List(("Daniel", 555)).toMap)
 
-  val names = List("Bob", "James", "Angela", "Mary", "Daniel", "Jim")
-  println(names.groupBy(name => name.charAt(0)))
+  val namesList = List("Bob", "James", "Angela", "Mary", "Daniel", "Jim")
+  val namesVector = Vector("Bob", "James", "Angela", "Mary", "Daniel", "Jim")
+  println(namesList.groupBy(name => name.charAt(0)))
 
+  /*
+    1. What would happen if I had two original entries "Jim" -> 555 and "JIM" -> 900?
+        !!! careful with mapping keys
+    2. Overly simplified social network based on maps
+       Person = String
+       - add a person to the network
+       - remove
+       - friend (mutual)
+       - unfriend (mutual)
+
+       - number of friends of a person
+       - person with most friends
+       - how many people have no friends
+       - if there is a social connection between two people (direct or not)
+   */
+  def add(network: Map[String, Set[String]], person: String): Map[String, Set[String]] =
+    network + (person -> Set())
+
+  def friend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+    val friendsA = network(a)
+    val friendsB = network(b)
+
+    network + (a -> (friendsA + b)) + (b -> (friendsB + a))
+  }
+  def unfriend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+    val friendsA = network(a)
+    val friendsB = network(b)
+
+    network + (a -> (friendsA - b)) + (b -> (friendsB - a))
 
 }
