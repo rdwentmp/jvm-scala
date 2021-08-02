dab10f0d9dd9575d6f430a967a398163214bcebc
rdwentmp <maaarek7823@gmail.com>
2021-08-02 11:04:05 +0200

    handling http Service

--- src/lectures/part3fp/HandlingFailure.scala
+++ src/lectures/part3fp/HandlingFailure.scala
@@ -1,6 +1,6 @@
 package lectures.part3fp
 
-import scala.util.{Try, Failure, Success}
+import scala.util.{Failure, Random, Success, Try}
 
 object HandlingFailure extends  App {
 
@@ -33,6 +33,36 @@ object HandlingFailure extends  App {
   // If you design the API
   def betterUnsafeMethod(): Try[String] = Failure(new RuntimeException)
   def betterBackupMethod(): Try[String] = Success("A valid result")
+  val betterFallback = betterUnsafeMethod() orElse betterBackupMethod()
 
+  // map, flatMap, filter
+  println(aSuccess.map(_ * 2))
+  println(aSuccess.flatMap(x => Success(x * 10)))
+  println(aSuccess.filter(_ > 10))
+
+  /*
+    Exercise
+   */
+  val hostname = "localhost"
+  val port = "8080"
+  def renderHTML(page: String) = println(page)
+
+  class Connection {
+    def get(ur: String): String = {
+      val random = new Random(System.nanoTime())
+      if (random.nextBoolean()) "<html>...</html>"
+      else throw new RuntimeException("connection interrupted")
+    }
+  }
+
+  object HttpService {
+    val random = new Random(System.nanoTime())
+
+    def getConnection(host: String, port: String): Connection = {
+      if (random.nextBoolean()) new Connection
+      else throw new RuntimeException("Someone else took the port")
+    }
+    // if you get the html page from the connection, print it to the console i.e call renderHTML
+  }
 
 }
