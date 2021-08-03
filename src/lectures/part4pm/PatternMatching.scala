fb1c5ee0e9ff07da90a4a242a8a4ad06449fba73
rdwentmp <maaarek7823@gmail.com>
2021-08-03 13:37:10 +0200

    Finish exercise PM

--- src/lectures/part4pm/PatternMatching.scala
+++ src/lectures/part4pm/PatternMatching.scala
@@ -53,5 +53,36 @@ object PatternMatching extends App {
   val isEvenCond = if(x % 2 == 0)true else false // /!
   val isEvenNormal = x % 2 == 0
 
+  /*
+  Exercise
+  simple function uses PM
+  takes an Expr => human readable form
 
+  Sum(Number(2)), Number(3)) => 2+3
+  Sum(Number(2)), Number(3), Number(4)) => 2+3+4
+  Prod(Sum(Number(2), Number(1)), Number(3)) = (2 + 1) * 3
+  Sum(Prod(Number(2), Number(1)), Number(3)) = 2 * 1 + 3
+   */
+  trait Expr
+  case class Number(n: Int) extends Expr
+  case class Sum(el: Expr, e2: Expr) extends Expr
+  case class Prod(el: Expr, e2: Expr) extends Expr
+
+  def show(e: Expr): String = e match {
+    case Number(n) => s"$n"
+    case Sum(e1, e2) => show(e1) + " + " + show(e2)
+    case Prod(e1, e2) => {
+      def maybeShowParentheses(exp: Expr) = exp match {
+        case Prod(_, _) => show(exp)
+        case Number(_) => show(exp)
+        case _ => "(" + show(exp) + ")"
+      }
+      maybeShowParentheses(e1) + " * " + maybeShowParentheses(e2)
+    }
+  }
+  println(show(Sum(Number(2), Number(3))))
+  println(show(Sum(Sum(Number(2), Number(3)), Number(4))))
+  println(show(Prod(Sum(Number(2), Number(1)), Number(3))))
+  println(show(Prod(Sum(Number(2), Number(1)), Sum(Number(3), Number(4)))))
+  println(show(Sum(Prod(Number(2), Number(1)), Number(3))))
 }
