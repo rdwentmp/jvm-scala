0e291959062959820d7ab4abfbfdee6cea90ee71
rdwentmp <maaarek7823@gmail.com>
2021-08-04 12:55:29 +0200

    all the patterns

--- src/lectures/part4pm/AllThePatterns.scala
+++ src/lectures/part4pm/AllThePatterns.scala
@@ -54,4 +54,35 @@ object AllThePatterns extends App {
     case List(1,2,3) :+ 42 => // infix pattern
   }
 
+  // 6 - type specifiers
+  val unknown: Any = 2
+  val unknownMatch = unknown match {
+    case list: List[Int] => // explicit type specifiers
+    case _ =>
+  }
+
+  // 7 - name biding
+//  val nameBindingMatch = aList match {
+//    case nonEmptyList @ Cons(_, _) => // name binding => use the name later(here)
+//    case Cons(1, rest @ Cons(2, _)) => // name binding inside nested patterns
+//  }
+
+  // 8 - multi-patterns
+//  val multiPattern = aList match {
+//    case Empty | Cons(0, _) => // compound pattern (multi-pattern)
+//  }
+
+  // 9 - if guards
+//  val secondElementSpecial = aList match {
+//    case Cons(_, Cons(specialElement, _)) if specialElement % 2 == 0 =>
+//  }
+
+  val numbers = List(1,2,3)
+  val numbersMatch = numbers match {
+    case listOfStrings: List[String] => "a list of strings"
+    case listOfNumbers: List[Int] => "a list of Numbers"
+    case _ => ""
+  }
+  println(numbersMatch)
+  // JVM trick question
 }
